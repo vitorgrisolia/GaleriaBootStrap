d5775ed49e602834dc17a2f277d66cded219ea54
vitorgrisolia <54046675+vitorgrisolia@users.noreply.github.com>
2021-09-29 15:02:21 -0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const modoDev = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    mode: modoDev ? 'development' : 'production',
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './build',
+        port: 9000,
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    nameCache: {},
+                }
+            }),
+            new CssMinimizerPlugin()
+        ]
+    },
+    output: {
+        filename: 'app.js',
+        path: __dirname + '/build'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'estilo.css' }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { context: 'src/', from: '**/*.html' },
+                { context: 'src/', from: 'imgs/**/*' }
+            ]
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // interpreta @import, url()...
+                'sass-loader',
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
+        }, {
+            test: /.(ttf|otf|eot|svg|woff(2)?)$/,
+            use: ['file-loader']
+        }]
+    }
+}
